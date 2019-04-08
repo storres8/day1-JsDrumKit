3ed967188f9aa1618a6f0cddab45fd7ee186084e
storres8 <stevetorres7562@gmail.com>
2019-04-08 13:00:30 -0400

    done.

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 window.addEventListener("keydown", e => {
   e.preventDefault();
   let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  let key = document.querySelector(`div[data-key="${e.keyCode}"]`);
   if (audio === null) {
     return;
   }
-  console.log(audio);
+  //   console.log(key);
   audio.play();
+  audio.currentTime = 0;
+  key.classList.add("playing");
+});
+
+function removeTrans(e) {
+  if (e.propertyName !== "transform") {
+    return;
+  }
+  this.classList.remove("playing");
+}
+
+let allKeys = document.querySelectorAll(".key");
+
+allKeys.forEach(key => {
+  key.addEventListener("transitionend", removeTrans);
 });
